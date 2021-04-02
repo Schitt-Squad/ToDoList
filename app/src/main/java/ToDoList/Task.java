ff2ff94999cd3bd372658bf388ae936bccbc88cf
AwesomeShadow36 <grovbrax@isu.edu>
2021-04-01 20:22:15 -0600

    Filled out Task and TaskList

--- app/src/main/java/ToDoList/Task.java
+++ app/src/main/java/ToDoList/Task.java
@@ -2,13 +2,14 @@ package ToDoList;
 
 import java.util.Calendar;
 
-public class Task {
+public class Task implements Cloneable {
     //Instance Variables
     private int priority;
     private String title;
     private String description;
     private Calendar dueDate;
     private String label;
+    private boolean completed;
 
     //Constructor
     public Task(int priority, String title, String description, Calendar dueDate, String label) {
@@ -17,6 +18,20 @@ public class Task {
         this.description = description;
         this.dueDate = dueDate;
         this.label = label;
+        this.completed= false;
+    }
+
+    public Task(int priority, String title, String description, String label) {
+        this.priority = priority;
+        this.title = title;
+        this.description = description;
+        this.dueDate = defaultDueDate();
+        this.label = label;
+        this.completed= false;
+    }
+
+    public Task(){
+
     }
 
     //Define getters and setters
@@ -62,13 +77,16 @@ public class Task {
 
     //Define default Due Date for Task
     public Calendar defaultDueDate(){
-        return null;
+        Calendar c= Calendar.getInstance();
+        c.add(Calendar.DATE, 7);
+        return c;
     }
 
-    //Allows user to duplicate a task
-    //Return type and parameters need to be changed
-    public boolean duplicateTask(){
-        return true;
+    //for duplication
+    public Task clone() throws CloneNotSupportedException{
+        return (Task)super.clone();
     }
+
+    public void markComplete(){completed=true;}
 
 }
