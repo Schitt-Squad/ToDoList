46b61cb7e0fed18224605e43b8a49c9c0773874a
Christian Liechty <liecchr2@isu.edu>
2021-04-02 21:16:46 -0600

    fleshed out Admin and worked on ToDoList facade

--- app/src/main/java/ToDoList/ToDoList.java
+++ app/src/main/java/ToDoList/ToDoList.java
@@ -4,4 +4,15 @@ public class ToDoList {
     //I don't think there needs to be anything more here
     private UserList UserL;
     private Admin admin;
+    private User currentUser;
+
+    //Review this method
+    public void login(User user){
+        currentUser = user;
+    }
+
+    //Review this method
+    public void logout(User user){
+        currentUser = null;
+    }
 }
